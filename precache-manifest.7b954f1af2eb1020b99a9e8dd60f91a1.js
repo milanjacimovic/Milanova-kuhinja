534e261b62631559214bd6c3ef74b2020e231f58
milanjacimovic <jacimovicmilanfon@gmail.com>
2020-04-03 11:29:38 +0200

    Updates

---
+++ precache-manifest.7b954f1af2eb1020b99a9e8dd60f91a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d1c325230bfce59eba14096dc16261",
+    "url": "/milanova-kuhinja/index.html"
+  },
+  {
+    "revision": "ecfa68c187e858d15b47",
+    "url": "/milanova-kuhinja/static/css/main.9df9b9c4.chunk.css"
+  },
+  {
+    "revision": "5b6cfd4e24434f74cb4f",
+    "url": "/milanova-kuhinja/static/js/2.b4e3051f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/milanova-kuhinja/static/js/2.b4e3051f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfa68c187e858d15b47",
+    "url": "/milanova-kuhinja/static/js/main.7d014b43.chunk.js"
+  },
+  {
+    "revision": "d11777cbe725c064bbff",
+    "url": "/milanova-kuhinja/static/js/runtime-main.e6f834b1.js"
+  },
+  {
+    "revision": "ef33443f888f9c162f76bcb32b5b6d1f",
+    "url": "/milanova-kuhinja/static/media/food.ef33443f.jpg"
+  },
+  {
+    "revision": "94fc5ae43c082f2bbc2426281febf5a5",
+    "url": "/milanova-kuhinja/static/media/recipe.94fc5ae4.jpg"
+  },
+  {
+    "revision": "84f3c4f3e129030ee5d9061ab734a17f",
+    "url": "/milanova-kuhinja/static/media/spinner.84f3c4f3.gif"
+  }
+]);
